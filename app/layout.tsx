c807e6811895cd74207047cb368cdc48bcb4504c
Muhammed Ibrahim <stringsaeed@gmail.com>
2025-03-22 06:20:33 +0400

    Remove unused UI components and update package dependencies; adjust global styles for improved aesthetics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Muhammed Saeed | React Native Engineer",
